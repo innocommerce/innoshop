2a941673363dec3b485fa4353205420ed8610cdd
Edward Yang <edward@innoshop.com>
2024-08-08 15:23:01 +0800

    optimize checkout payment

--- innopacks/front/resources/views/orders/pay.blade.php
+++ innopacks/front/resources/views/orders/pay.blade.php
@@ -12,17 +12,36 @@
       {{ $error }}
     @endif
 
-    @if(isset($payment_view))
-    {!! $payment_view !!}
-    @endif
+    <table class="table w-max-800 mx-auto mb-3 mb-md-5 checkout-success-table">
+      <thead>
+      <tr>
+        <th>{{ __('front::order.order_number') }}</th>
+        <th>{{ __('front::order.order_billing') }}</th>
+        <th>{{ __('front::order.order_total') }}</th>
+        <th>{{ __('front::order.order_status') }}</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+        <td>{{ $order->number }}</td>
+        <td>{{ $order->billing_method_name }}</td>
+        <td>{{ currency_format($order->total) }}</td>
+        <td>{{ $order->status_format }}</td>
+      </tr>
+      </tbody>
+    </table>
+
+    <div class="d-flex flex-column justify-content-center w-max-800 mx-auto">
+      @if(isset($payment_view))
+        {!! $payment_view !!}
+      @endif
+    </div>
   </div>
 
-  @hookinsert('order.pay.top')
+  @hookinsert('order.pay.bottom')
 
 @endsection
 
 @push('footer')
-  <script>
-
-  </script>
+  <script></script>
 @endpush
