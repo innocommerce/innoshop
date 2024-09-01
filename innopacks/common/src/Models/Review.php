348e6c4df2b4998e757833525eee498120418d0e
Edward Yang <edward@innoshop.com>
2024-09-01 11:48:01 +0800

    add order review relations

--- innopacks/common/src/Models/Review.php
+++ innopacks/common/src/Models/Review.php
@@ -9,6 +9,9 @@
 
 namespace InnoShop\Common\Models;
 
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use InnoShop\Common\Models\Order\Item;
+
 class Review extends BaseModel
 {
     protected $table = 'reviews';
@@ -16,4 +19,28 @@ class Review extends BaseModel
     protected $fillable = [
         'customer_id', 'product_id', 'order_item_id', 'rating', 'title', 'content', 'like', 'dislike', 'active',
     ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function customer(): BelongsTo
+    {
+        return $this->belongsTo(Customer::class, 'customer_id', 'id');
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class, 'product_id', 'id');
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function orderItem(): BelongsTo
+    {
+        return $this->belongsTo(Item::class, 'order_item_id', 'id');
+    }
 }
