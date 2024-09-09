75ff3d3fbdec1cb904bdf6c3846fe124720d8172
Edward Yang <edward@innoshop.com>
2024-09-09 18:35:28 +0800

    release 0.3.0

--- innopacks/common/config/innoshop.php
+++ innopacks/common/config/innoshop.php
@@ -8,7 +8,7 @@
  */
 
 return [
-    'version' => '0.3.1',
-    'build'   => '20240908',
+    'version' => '0.3.0',
+    'build'   => '20240909',
     'api_url' => env('INNOSHOP_API_URL', 'https://www.innoshop.cn'),
 ];
