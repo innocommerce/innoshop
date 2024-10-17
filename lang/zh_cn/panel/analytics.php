a8d4b199b25e065085550ddb374c623362bf6952
Edward Yang <edward@innoshop.com>
2024-10-17 09:14:42 +0800

    Sort the Chinese language package by key.

--- lang/zh_cn/panel/analytics.php
+++ lang/zh_cn/panel/analytics.php
@@ -8,7 +8,7 @@
  */
 
 return [
-    'product_trends'  => '产品发布',
     'customer_trends' => '用户注册',
+    'product_trends'  => '产品发布',
     'total_trends'    => '订单总额',
 ];
--- lang/zh_cn/panel/login.php
+++ lang/zh_cn/panel/login.php
@@ -8,10 +8,10 @@
  */
 
 return [
-    'title'       => 'InnoShop 后台管理',
-    'keywords'    => 'InnoShop, 创新, 开源, 电商, Laravel, 多语言, 多货币, Hook, 插件架构, 灵活, 强大',
     'description' => 'InnoShop, 创新开源电商系统',
-    'login_index' => 'InnoShop 后台登录',
     'email'       => '邮箱',
+    'keywords'    => 'InnoShop, 创新, 开源, 电商, Laravel, 多语言, 多货币, Hook, 插件架构, 灵活, 强大',
+    'login_index' => 'InnoShop 后台登录',
     'password'    => '密码',
+    'title'       => 'InnoShop 后台管理',
 ];
