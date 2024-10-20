c56e1fc40a5539a3848cc35d7af0908817d79d9a
Edward Yang <edward@innoshop.com>
2024-10-20 21:46:47 +0800

    update es and ja

--- lang/ja/common/address.php
+++ lang/ja/common/address.php
@@ -8,16 +8,17 @@
  */
 
 return [
-    'address'          => '住所',
     'add_new_address'  => '新しい住所を追加',
-    'shipping_address' => '配送先住所',
-    'billing_address'  => '請求先住所',
-    'name'             => '名前',
+    'address'          => '住所',
     'address_1'        => '住所1',
     'address_2'        => '住所2',
-    'zipcode'          => '郵便番号',
+    'billing_address'  => '請求先住所',
     'city'             => '市区町村',
     'country'          => '国',
-    'state'            => '州',
+    'email'            => 'メール',
+    'name'             => '名前',
     'phone'            => '電話番号',
+    'shipping_address' => '配送先住所',
+    'state'            => '州',
+    'zipcode'          => '郵便番号',
 ];
