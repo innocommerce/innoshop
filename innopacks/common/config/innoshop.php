0b73559dbe57f11eed0fc4471717ef6fd9b3281e
yushine <edward@innoshop.com>
2024-10-23 13:56:42 +0800

    update innoshop version

--- innopacks/common/config/innoshop.php
+++ innopacks/common/config/innoshop.php
@@ -8,7 +8,7 @@
  */
 
 return [
-    'version' => '0.3.2',
-    'build'   => '20240929',
+    'version' => '0.3.5',
+    'build'   => '20241023',
     'api_url' => env('INNOSHOP_API_URL', 'https://www.innoshop.cn'),
 ];
