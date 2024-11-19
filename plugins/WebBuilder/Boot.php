4fa7cc22049bb3620c1f727f57dd3798b1bd2dd4
yushine <edward@innoshop.com>
2024-11-19 13:52:51 +0800

    init web builder

---
+++ plugins/WebBuilder/Boot.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Copyright (c) Since 2024 InnoShop - All Rights Reserved
+ *
+ * @link       https://www.innoshop.com
+ * @author     InnoShop <team@innoshop.com>
+ * @license    https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ */
+
+namespace Plugin\WebBuilder;
+
+use InnoShop\Plugin\Core\BaseBoot;
+
+class Boot extends BaseBoot
+{
+    /**
+     * @return void
+     */
+    public function init(): void
+    {
+        $this->addPanelMenus();
+    }
+
+    /**
+     * @return void
+     */
+    private function addPanelMenus(): void
+    {
+        listen_hook_filter('component.sidebar.design.routes', function ($data) {
+            $data[] = [
+                'route' => 'web_builder.index',
+                'title' => trans('WebBuilder::route.title'),
+            ];
+
+            return $data;
+        });
+    }
+}
---
+++ plugins/WebBuilder/Controllers/WebBuilderController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Copyright (c) Since 2024 InnoShop - All Rights Reserved
+ *
+ * @link       https://www.innoshop.com
+ * @author     InnoShop <team@innoshop.com>
+ * @license    https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ */
+
+namespace Plugin\WebBuilder\Controllers;
+
+use InnoShop\Panel\Controllers\BaseController;
+
+class WebBuilderController extends BaseController
+{
+    public function index()
+    {
+        dump(__FILE__, __METHOD__);
+    }
+}
