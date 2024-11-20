9e2755f9f1b6b10b9c583de0d61389bec7ddb552
yushine <edward@innoshop.com>
2024-11-20 11:18:52 +0800

    set brand logo required

--- innopacks/panel/resources/views/brands/form.blade.php
+++ innopacks/panel/resources/views/brands/form.blade.php
@@ -16,10 +16,10 @@
       @csrf
       @method($brand->id ? 'PUT' : 'POST')
 
-      <x-common-form-input title="名称" name="name" value="{{ old('name', $brand->name) }}" required placeholder="名称" />
-      <x-common-form-image title="图标" name="logo" value="{{ old('logo', $brand->logo) }}" required />
-      <x-common-form-input title="首字母" name="first" value="{{ old('first', $brand->first) }}" required placeholder="首字母" />
-      <x-common-form-input title="排序" name="position" value="{{ old('position', $brand->position) }}" placeholder="排序" />
+      <x-common-form-input title="{{ __('panel/brand.name') }}" name="name" value="{{ old('name', $brand->name) }}" required placeholder="{{ __('panel/brand.name') }}" />
+      <x-common-form-image title="{{ __('panel/brand.logo') }}" name="logo" value="{{ old('logo', $brand->logo) }}" required />
+      <x-common-form-input title="{{ __('panel/brand.first') }}" name="first" value="{{ old('first', $brand->first) }}" required placeholder="{{ __('panel/brand.first') }}" />
+      <x-common-form-input title="{{ __('panel/common.position') }}" name="position" value="{{ old('position', $brand->position) }}" placeholder="{{ __('panel/common.position') }}" />
       <x-common-form-switch-radio title="{{ __('panel/common.whether_enable') }}" name="active" :value="old('active', $page->active ?? true)" placeholder="{{ __('panel/common.whether_enable') }}"/>
       <x-common-form-input title="{{ __('panel/common.slug') }}" name="slug" value="{{ old('slug', $brand->slug) }}" placeholder="{{ __('panel/common.slug') }}" />
 
