193f61435c37028a1b54bed5dea7355776ba393f
jack <jacky@innoshop.com>
2024-12-18 11:37:14 +0800

    fix naming notations

--- innopacks/panel/resources/views/products/form.blade.php
+++ innopacks/panel/resources/views/products/form.blade.php
@@ -25,37 +25,37 @@ $weightClasses = [
     <div class="card-body">
      <ul class="nav nav-tabs" id="myTab" role="tablist">
       <li class="nav-item" role="presentation">
-       <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home-tab-pane" type="button"
-        role="tab" aria-controls="home-tab-pane" aria-selected="true">{{ __('panel/product.basic_information') }}</button>
+       <button class="nav-link active" id="basic-tab" data-bs-toggle="tab" data-bs-target="#basic-tab-pane" type="button"
+        role="tab" aria-controls="basic-tab-pane" aria-selected="true">{{ __('panel/product.basic_information') }}</button>
       </li>
       <li class="nav-item" role="presentation">
-       <button class="nav-link" id="description-tab" data-bs-toggle="tab" data-bs-target="#description-tab-pane"
-        type="button" role="tab" aria-controls="description-tab-pane" aria-selected="false">{{__('panel/product.product_description')}}</button>
+       <button class="nav-link" id="translation-tab" data-bs-toggle="tab" data-bs-target="#translation-tab-pane"
+        type="button" role="tab" aria-controls="translation-tab-pane" aria-selected="false">{{__('panel/product.product_description')}}</button>
       </li>
       <li class="nav-item" role="presentation">
-       <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile-tab-pane" type="button"
-        role="tab" aria-controls="profile-tab-pane" aria-selected="false">{{__('panel/product.specification_attribute')}}</button>
+       <button class="nav-link" id="specification-tab" data-bs-toggle="tab" data-bs-target="#specification-tab-pane" type="button"
+        role="tab" aria-controls="specification-tab-pane" aria-selected="false">{{__('panel/product.specification_attribute')}}</button>
       </li>
       <li class="nav-item" role="presentation">
-       <button class="nav-link" id="promotion-tab" data-bs-toggle="tab" data-bs-target="#promotion-tab-pane"
-        type="button" role="tab" aria-controls="promotion-tab-pane" aria-selected="false">{{__('panel/product.extend_information')}}</button>
+       <button class="nav-link" id="addition-tab" data-bs-toggle="tab" data-bs-target="#addition-tab-pane"
+        type="button" role="tab" aria-controls="addition-tab-pane" aria-selected="false">{{__('panel/product.extend_information')}}</button>
       </li>
       <li class="nav-item" role="presentation">
-       <button class="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact-tab-pane" type="button"
-        role="tab" aria-controls="contact-tab-pane" aria-selected="false">{{__('panel/product.seo')}}</button>
+       <button class="nav-link" id="seo-tab" data-bs-toggle="tab" data-bs-target="#seo-tab-pane" type="button"
+        role="tab" aria-controls="seo-tab-pane" aria-selected="false">{{__('panel/product.seo')}}</button>
       </li>
       <li class="nav-item" role="presentation">
-       <button class="nav-link" id="related-tab" data-bs-toggle="tab" data-bs-target="#related-tab-pane" type="button"
-        role="tab" aria-controls="related-tab-pane" aria-selected="false">{{__('panel/product.related_products')}}</button>
+       <button class="nav-link" id="relation-tab" data-bs-toggle="tab" data-bs-target="#relation-tab-pane" type="button"
+        role="tab" aria-controls="relation-tab-pane" aria-selected="false">{{__('panel/product.related_products')}}</button>
       </li>
       <li class="nav-item" role="presentation">
-       <button class="nav-link" id="other-tab" data-bs-toggle="tab" data-bs-target="#other-tab-pane" type="button"
-        role="tab" aria-controls="other-tab-pane" aria-selected="false">{{__('panel/product.promotional_discount')}}</button>
+       <button class="nav-link" id="promotion-tab" data-bs-toggle="tab" data-bs-target="#promotion-tab-pane" type="button"
+        role="tab" aria-controls="promotion-tab-pane" aria-selected="false">{{__('panel/product.promotional_discount')}}</button>
       </li>
      </ul>
 
      <div class="tab-content" id="myTabContent">
-      <div class="tab-pane fade show active mt-3" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab" tabindex="0">
+      <div class="tab-pane fade show active mt-3" id="basic-tab-pane" role="tabpanel" aria-labelledby="basic-tab" tabindex="0">
        <div class="mb-3">
         <div class="mb-1 fs-6">{{ __('panel/product.name') }}</div>
         @foreach (locales() as $locale)
@@ -81,7 +81,7 @@ $weightClasses = [
        <x-common-form-switch-radio :title="__('panel/common.status')" name="active"
         :value="old('active', $product->active ?? true)" />
       </div>
-      <div class="tab-pane fade mt-4" id="description-tab-pane" role="tabpanel" aria-labelledby="description-tab"
+      <div class="tab-pane fade mt-4" id="translation-tab-pane" role="tabpanel" aria-labelledby="translation-tab"
        tabindex="1">
        <ul class="nav nav-tabs" id="myTab" role="tablist">
         @foreach (locales() as $locale)
@@ -100,7 +100,7 @@ $weightClasses = [
         @endforeach
        </ul>
 
-       <div class="tab-content border p-2" id="myTabContent">
+       <div class="tab-content border-end border-bottom border-start" id="myTabContent">
         @foreach (locales() as $locale)
         @php($localeCode = $locale->code)
         @php($localeName = $locale->name)
@@ -120,7 +120,7 @@ $weightClasses = [
         @endforeach
        </div>
       </div>
-      <div class="tab-pane fade mt-4" id="profile-tab-pane" role="tabpanel" aria-labelledby="profile-tab" tabindex="2">
+      <div class="tab-pane fade mt-4" id="specification-tab-pane" role="tabpanel" aria-labelledby="specification-tab" tabindex="2">
        <div class="mt-2 skus-single-box {{ $product->is_multiple() ? 'd-none' : '' }}">
         <div class="alert alert-info mb-3">
          <i class="bi bi-info-circle me-2"></i>
@@ -155,7 +155,7 @@ $weightClasses = [
        @include('panel::products._form_variant')
        @include('panel::products._form_attribute')
       </div>
-      <div class="tab-pane fade mt-3" id="promotion-tab-pane" role="tabpanel" aria-labelledby="promotion-tab" tabindex="3">
+      <div class="tab-pane fade mt-3" id="addition-tab-pane" role="tabpanel" aria-labelledby="addition-tab" tabindex="3">
        <div class="d-flex flex-column flex-sm-row gap-3">
         <div class="col-12 col-sm-6">
          <x-common-form-select :title="__('panel/product.tax_class')" name="tax_class_id"
@@ -200,7 +200,7 @@ $weightClasses = [
        <x-common-form-switch-radio :title="__('panel/product.is_virtual')" name="is_virtual"
         :value="old('is_virtual', $product->is_virtual ?? false)" />
       </div>
-      <div class="tab-pane fade mt-3" id="contact-tab-pane" role="tabpanel" aria-labelledby="contact-tab" tabindex="4">
+      <div class="tab-pane fade mt-3" id="seo-tab-pane" role="tabpanel" aria-labelledby="seo-tab" tabindex="4">
        <x-common-form-input :title="__('panel/common.slug')" name="slug" :value="old('slug', $product->slug ?? '')"
         :placeholder="__('panel/common.slug')" column="product_slug" generate="true" />
        <ul class="nav nav-tabs mt-3 " id="myTab" role="tablist">
@@ -266,11 +266,11 @@ $weightClasses = [
         @endforeach
        </div>
       </div>
-      <div class="tab-pane fade mt-3" id="related-tab-pane" role="tabpanel" aria-labelledby="related-tab" tabindex="5">
+      <div class="tab-pane fade mt-3" id="relation-tab-pane" role="tabpanel" aria-labelledby="relation-tab" tabindex="5">
        <x-panel-form-autocomplete-list name="related_ids[]" :value="old('related_ids', $product->relations->pluck('relation_id')->toArray() ?? [])" placeholder="{{ __('panel/product.searching_products') }}"
                                        title="{{__('panel/product.related_products')}}" api="/api/panel/products" />
       </div>
-      <div class="tab-pane fade mt-3" id="other-tab-pane" role="tabpanel" aria-labelledby="other-tab" tabindex="6">
+      <div class="tab-pane fade mt-3" id="promotion-tab-pane" role="tabpanel" aria-labelledby="promotion-tab" tabindex="6">
        特价
       </div>
      </div>
