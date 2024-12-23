4974e8398ca0c28335d163848929b28b5324339d
xslong <xiaoshaolong@126.com>
2024-12-23 10:33:02 +0800

    Customer Filter Support Group

--- innopacks/panel/resources/views/components/criteria.blade.php
+++ innopacks/panel/resources/views/components/criteria.blade.php
@@ -18,7 +18,8 @@
             <select name="{{ $item['name'] }}" id="{{ $item['name'] }}" class="form-select form-select-sm">
               <option value="">{{ panel_trans('common.please_choose') }}</option>
               @foreach($item['options'] as $option)
-                <option value="{{ $option['value'] }}" {{ request($item['name']) == $option['value'] ? 'selected' : '' }}>{{ $option['label'] }}</option>
+                <option value="{{ isset($item['options_key']) ? $option[$item['options_key']] : $option['value'] }}" {{ request($item['name'])==(isset($item['options_key']) ? $option[$item['options_key']] : $option['value']) ? 'selected' : '' }}>{{
+                  isset($option['label_key']) ? trans($option['label_key']) : (isset($item['options_label']) ? $option[$item['options_label']] : $option['label']) }}</option>
               @endforeach
             </select>
           </div>
