2eac10d5e1edcef1303fde9fad668dc9309de6fc
Edward Yang <edward@innoshop.com>
2025-01-13 10:22:19 +0800

    fixed blade hook with output
    fixed hook output

--- innopacks/plugin/src/Core/Blade/Hook.php
+++ innopacks/plugin/src/Core/Blade/Hook.php
@@ -12,6 +12,7 @@
 namespace InnoShop\Plugin\Core\Blade;
 
 use Barryvdh\Debugbar\Facades\Debugbar;
+use Exception;
 use Illuminate\Support\Arr;
 
 class Hook
@@ -23,6 +24,8 @@ class Hook
     protected array $stop = [];
 
     protected array $mock = [];
+
+    protected array $outputs = [];
 
     protected bool $isUpdate = false;
 
@@ -53,6 +56,7 @@ class Hook
      * @param  string  $htmlContent  content wrapped by hook
      *
      * @return null|void
+     * @throws Exception
      */
     public function get(string $hook, array $params = [], ?callable $callback = null, string $htmlContent = '')
     {
@@ -80,6 +84,7 @@ class Hook
      * @param  callable|null  $callback
      * @param  string  $htmlContent
      * @return string|null
+     * @throws Exception
      */
     public function getHook(string $hook, array $params = [], ?callable $callback = null, string $htmlContent = ''): ?string
     {
@@ -98,6 +103,7 @@ class Hook
      * @param  callable|null  $callback
      * @param  string  $htmlContent
      * @return string|null
+     * @throws Exception
      */
     public function getWrapper(string $hook, array $params = [], ?callable $callback = null, string $htmlContent = ''): ?string
     {
@@ -243,6 +249,7 @@ class Hook
      * @param  string|null  $output  html wrapped by hook
      *
      * @return mixed
+     * @throws Exception
      */
     protected function run(string $hook, array $params, Callback $callback, ?string $output = null): mixed
     {
@@ -253,6 +260,8 @@ class Hook
         if ($this->withCallback) {
             array_unshift($params, $callback);
         }
+
+        $this->outputs[$hook] = $output;
 
         if (array_key_exists($hook, $this->watch)) {
             if (is_array($this->watch[$hook])) {
@@ -265,12 +274,16 @@ class Hook
 
                     $output = call_user_func_array($function['function'], $params);
 
-                    $params[1] = $output;
+                    $this->outputs[$hook] .= $output;
+
+                    if ($this->isUpdate) {
+                        $params[1] = $output;
+                    }
                 }
             }
         }
 
-        return $output;
+        return $this->outputs[$hook] ?? '';
     }
 
     /**
