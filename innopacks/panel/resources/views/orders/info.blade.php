ecb6d5d6758014c9532510fa075afa177d131b60
Edward Yang <edward@innoshop.com>
2025-01-13 16:14:20 +0800

    fixed history comment

--- innopacks/panel/resources/views/orders/info.blade.php
+++ innopacks/panel/resources/views/orders/info.blade.php
@@ -156,43 +156,46 @@
 
   <div class="mt-4">
     <div class="card mb-4">
-        <div class="card-header">
-            <h5 class="card-title mb-0">{{ __('front/checkout.order_comment') }}</h5>
-        </div>
-        <div class="card-body">
-            <div class="row">
-                <div class="col-12 col-md-6 mb-4"> 
-                    <h6 class="fs-5">{{ __('panel/order.customer_remarks') }}</h6>
-                    <p class="mb-0">{{ $order->comment }}</p>
-                </div>
-                <div class="col-12 col-md-6 mb-3">
-                    <h6 class="fs-5">{{ __('panel/order.administrator_remarks') }}</h6>
-                    <p class="mb-0">{{ $order->admin_note }}</p>
-                    <button class="btn btn-sm btn-primary mt-2" data-bs-toggle="modal" data-bs-target="#admin_note">
-                       {{ __('panel/common.edit') }}
-                    </button>
+      <div class="card-header">
+        <h5 class="card-title mb-0">{{ __('front/checkout.order_comment') }}</h5>
+      </div>
+      <div class="card-body">
+        <div class="row">
+          <div class="col-12 col-md-6 mb-4">
+            <h6 class="fs-5">{{ __('panel/order.customer_remarks') }}</h6>
+            <p class="mb-0">{{ $order->comment }}</p>
+          </div>
+          <div class="col-12 col-md-6 mb-3">
+            <h6 class="fs-5">{{ __('panel/order.administrator_remarks') }}</h6>
+            <p class="mb-0">{{ $order->admin_note }}</p>
+            <button class="btn btn-sm btn-primary mt-2" data-bs-toggle="modal" data-bs-target="#admin_note">
+              {{ __('panel/common.edit') }}
+            </button>
 
-                    <div class="modal fade" id="admin_note" tabindex="-1" aria-labelledby="admin_noteLabel" aria-hidden="true">
-                        <div class="modal-dialog modal-dialog-centered">
-                            <div class="modal-content border border-secondary rounded">
-                                <div class="modal-header">
-                                    <h4 class="modal-title" id="admin_noteLabel">{{ __('panel/order.administrator_remarks') }}</h4>
-                                </div>
-                                <div class="modal-body">
-                                    <textarea class="form-control admin-comment-input" rows="5" data-order-id="{{ $order->id }}">{{ $order->admin_note }}</textarea>
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-default" data-bs-dismiss="modal">{{ __('panel/order.close') }}</button>
-                                    <button type="button" class="btn btn-primary" onclick="submitComment()">{{ __('panel/order.submit') }}</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+            <div class="modal fade" id="admin_note" tabindex="-1" aria-labelledby="admin_noteLabel" aria-hidden="true">
+              <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content border border-secondary rounded">
+                  <div class="modal-header">
+                    <h4 class="modal-title" id="admin_noteLabel">{{ __('panel/order.administrator_remarks') }}</h4>
+                  </div>
+                  <div class="modal-body">
+                    <textarea class="form-control admin-comment-input" rows="5"
+                              data-order-id="{{ $order->id }}">{{ $order->admin_note }}</textarea>
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-default"
+                            data-bs-dismiss="modal">{{ __('panel/order.close') }}</button>
+                    <button type="button" class="btn btn-primary"
+                            onclick="submitComment()">{{ __('panel/order.submit') }}</button>
+                  </div>
                 </div>
+              </div>
             </div>
+          </div>
         </div>
+      </div>
     </div>
-</div>
+  </div>
 
   <div class="card mb-4">
     <div class="card-header">
@@ -211,7 +214,7 @@
         @foreach($order->histories as $history)
           <tr>
             <td data-title="State">{{ $history->status }}</td>
-            <td data-title="Remark">{{ $order->comment }}</td>
+            <td data-title="Remark">{{ $history->comment }}</td>
             <td data-title="Update Time">{{ $history->created_at }}</td>
           </tr>
         @endforeach
