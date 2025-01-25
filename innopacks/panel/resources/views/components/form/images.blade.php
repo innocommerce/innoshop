9519fd3b999af0f450516c032db6ba329f2e0309
Sam Xing <360338521@qq.com>
2025-01-25 15:22:21 +0800

    feat: upload images function use FileManger

--- innopacks/panel/resources/views/components/form/images.blade.php
+++ innopacks/panel/resources/views/components/form/images.blade.php
@@ -53,71 +53,24 @@
 <script>
   $('.img-upload-trigger').click(function () {
     const _self = $(this);
-    $('#form-upload').remove();
-    $('body').prepend('<form enctype="multipart/form-data" id="form-upload" style="display: none;"><input type="file" accept="image/*" multiple name="file" /></form>');
-    $('#form-upload input[name=\'file\']').trigger('click');
-    $('#form-upload input[name=\'file\']').change(function () {
-      let file = $(this).prop('files');
-      imgsUploadAjax(file, _self);
-    });
-  })
+    const upFile = _self.parents('.is-up-file');
+    const imgMax = upFile.data('img-max');
+    const currentCount = upFile.find('.img-upload-item').length - 1; // 减去触发器本身
 
-  // 允许拖拽上传
-  $('.is-up-file').on('dragover', function (e) {
-    e.preventDefault();
-    e.stopPropagation();
-    $(this).addClass('border-primary');
-  });
+    // 调用文件管理器
+    window.inno.fileManagerIframe((files) => {
+      // 确保files是数组
+      const fileArray = Array.isArray(files) ? files : [files];
 
-  // dragleave
-  $('.is-up-file').on('dragleave', function (e) {
-    e.preventDefault();
-    e.stopPropagation();
-    $(this).removeClass('border-primary');
-  });
+      // 处理选中的文件
+      fileArray.forEach((file, index) => {
+        if (imgMax && (currentCount + index) >= imgMax) {
+          return;
+        }
 
-  $('.is-up-file').on('drop', function (e) {
-    e.preventDefault();
-    e.stopPropagation();
-    let file = e.originalEvent.dataTransfer.files;
-    imgsUploadAjax(file, $(this));
-    $(this).removeClass('border-primary');
-  });
+        let val = file.path;
+        let url = file.url;
 
-  $('.is-up-file').on('click', '.delete-img', function () {
-    let count = $(this).parents('.is-up-file').find('.imgs-count');
-    count.text(count.text() * 1 - 1);
-    $(this).parent().parent().remove();
-  });
-
-  $('.is-up-file').on('click', '.show-img', function () {
-    let src = $(this).parent().next().find('img').data('origin-img');
-    let img = '<img src="' + src + '" class="img-fluid">';
-    $('#modal-show-imgs .modal-body').html(img);
-    $('#modal-show-imgs').modal('show');
-  });
-
-  function imgsUploadAjax(file, _self) {
-    let imgsCount = _self.parents('.is-up-file').find('.imgs-count').text() * 1;
-    let imgMax = _self.parents('.is-up-file').data('img-max');
-
-    for (let i = 0; i < file.length; i++) {
-      if (file[i].type.indexOf('image') === -1) {
-        continue;
-      }
-
-      if (imgMax && imgsCount >= imgMax) {
-        return;
-      }
-
-      let formData = new FormData();
-      formData.append('image', file[i]);
-      formData.append('type', _self.parents('.is-up-file').data('type'));
-
-      _self.find('.img-loading').removeClass('d-none');
-      axios.post(urls.upload_images, formData, {}).then(function (res) {
-        let val = res.data.value;
-        let url = res.data.url;
         let item = '<div class="img-upload-item wh-80 position-relative d-flex justify-content-center rounded overflow-hidden align-items-center border border-1 mb-1 me-1">';
         item += '<div class="position-absolute tool-wrap d-flex top-0 start-0 w-100 bg-primary bg-opacity-75"><div class="show-img w-100 text-center"><i class="bi bi-eye text-white"></i></div><div class="w-100 delete-img text-center"><i class="bi bi-trash text-white"></i></div></div>';
         item += '<div class="img-info d-flex justify-content-center align-items-center h-100 w-80 bg-white">';
@@ -125,16 +78,31 @@
         item += '</div>';
         item += '<input class="d-none" name="{{ $name }}[]" value="' + val + '">';
         item += '</div>';
+
         _self.before(item);
-        _self.parents('.is-up-file').find('.imgs-count').text(_self.parents('.is-up-file').find('.img-upload-item').length - 1);
-      }).catch(function (err) {
-        inno.msg(err.response.data.message);
-      }).finally(function () {
-        _self.find('.img-loading').addClass('d-none');
       });
 
-      imgsCount++
-    }
-  }
+      // 更新计数
+      upFile.find('.imgs-count').text(upFile.find('.img-upload-item').length - 1);
+    }, {
+      multiple: true,
+      type: 'image'
+    });
+  });
+
+  // 删除图片
+  $('.is-up-file').on('click', '.delete-img', function () {
+    let count = $(this).parents('.is-up-file').find('.imgs-count');
+    count.text(count.text() * 1 - 1);
+    $(this).parent().parent().remove();
+  });
+
+  // 预览图片
+  $('.is-up-file').on('click', '.show-img', function () {
+    let src = $(this).parent().next().find('img').data('origin-img');
+    let img = '<img src="' + src + '" class="img-fluid">';
+    $('#modal-show-imgs .modal-body').html(img);
+    $('#modal-show-imgs').modal('show');
+  });
 </script>
 @endPushOnce
