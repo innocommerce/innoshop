bcf148cfbc256272b592f5093357425f061dcf23
Edward Yang <edward@innoshop.com>
2025-02-06 11:29:55 +0800

    change version to 0.3.9

--- innopacks/common/config/innoshop.php
+++ innopacks/common/config/innoshop.php
@@ -9,7 +9,7 @@
 
 return [
     'edition' => 'community',
-    'version' => '0.3.8',
-    'build'   => '20241223',
+    'version' => '0.3.9',
+    'build'   => '20250206',
     'api_url' => env('INNOSHOP_API_URL', 'https://www.innoshop.cn'),
 ];
