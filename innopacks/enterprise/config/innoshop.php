b55162f9ee8c54be5167fc22290130dfde13b856
Edward Yang <yangjin@opencart.cn>
2025-03-19 10:26:01 +0800

    release v0.4.0 202503018

--- innopacks/enterprise/config/innoshop.php
+++ innopacks/enterprise/config/innoshop.php
@@ -9,7 +9,7 @@
 
 return [
     'edition' => 'enterprise',
-    'version' => '0.4.1',
+    'version' => '0.4.0',
     'build'   => '202503018',
     'api_url' => env('INNOSHOP_API_URL', 'https://www.innoshop.cn'),
 ];
