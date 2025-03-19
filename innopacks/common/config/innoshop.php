8110c615cdcb81a4e1f72763cc9a096818267aac
yushine <edward@innoshop.com>
2025-03-19 16:45:02 +0800

    0.4.0-beta2

--- innopacks/common/config/innoshop.php
+++ innopacks/common/config/innoshop.php
@@ -9,7 +9,7 @@
 
 return [
     'edition' => 'community',
-    'version' => '0.4.0-beta1',
+    'version' => '0.4.0-beta2',
     'build'   => '20250308',
     'api_url' => env('INNOSHOP_API_URL', 'https://www.innoshop.cn'),
 ];
