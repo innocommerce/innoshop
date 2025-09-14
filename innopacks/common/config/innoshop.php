a5b6b6563dca33cf81546c0ee9e8b0e2eeefaff9
Edward Yang <edward@innoshop.com>
2025-09-14 16:30:30 +0800

    update variant value editor font

--- innopacks/common/config/innoshop.php
+++ innopacks/common/config/innoshop.php
@@ -9,7 +9,7 @@
 
 return [
     'edition' => 'community',
-    'version' => '0.5.8',
-    'build'   => '20250818',
+    'version' => '0.5.9',
+    'build'   => '20250915',
     'api_url' => env('INNOSHOP_API_URL', 'https://www.innoshop.cn'),
 ];
