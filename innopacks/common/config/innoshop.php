8a0a0cd5b84190c8f61580e2246d20a163e92169
Edward Yang <edward@innoshop.com>
2025-12-15 17:11:19 +0800

    release community 0.6.3(20251215)

--- innopacks/common/config/innoshop.php
+++ innopacks/common/config/innoshop.php
@@ -9,7 +9,7 @@
 
 return [
     'edition' => 'community',
-    'version' => '0.6.2',
-    'build'   => '20251204',
+    'version' => '0.6.3',
+    'build'   => '20251215',
     'api_url' => env('INNOSHOP_API_URL', 'https://www.innoshop.cn'),
 ];
