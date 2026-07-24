57f4b7aadb4e284a0e0c1c9360b55e831188300b
Edward <edward@innoshop.com>
2026-07-24 17:41:26 +0800

    release: bump version to 0.9.0 (build 20260724)

--- innopacks/common/config/innoshop.php
+++ innopacks/common/config/innoshop.php
@@ -9,8 +9,8 @@
 
 return [
     'edition' => 'community',
-    'version' => '0.8.7',
-    'build'   => '20260722',
+    'version' => '0.9.0',
+    'build'   => '20260724',
     'api_url' => env('INNOSHOP_API_URL', 'https://www.innoshop.cn'),
 
     // Override GeoLite2-City.mmdb path via GEOLITE2_PATH; defaults to storage/app/geolite2/geolite2-city.mmdb.
